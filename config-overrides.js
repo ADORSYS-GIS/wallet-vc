e14f725c7a26c13f4fa5f78e9dd2bbb90316e1e9
forkimenjeckayang <forkimenjeckayang@gmail.com>
2025-02-12 18:50:44 +0100

    package.json update with config-overwrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,34 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable no-undef */
 const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
 
+const appDirectory = fs.realpathSync(process.cwd());
+const resolvePackage = (relativePath) =>
+  path.resolve(appDirectory, relativePath);
+
+const transpileModules = [
+  resolvePackage('node_modules/didcomm'),
+  resolvePackage('node_modules/did-resolver-lib'),
+];
+
+const modifyBabelLoader = (rule) => {
+  if (rule.loader && rule.loader.includes('babel-loader')) {
+    rule.include = [].concat(rule.include || [], transpileModules);
+    if (rule.exclude && rule.exclude instanceof RegExp) {
+      const originalExclude = rule.exclude;
+      rule.exclude = (filepath) =>
+        transpileModules.some((modPath) => filepath.startsWith(modPath))
+          ? false
+          : originalExclude.test(filepath);
+    }
+  }
+};
+
+// eslint-disable-next-line no-undef
 module.exports = {
   webpack: (config) => {
-    // Ensure resolve.fallback exists before adding polyfills
     config.resolve.fallback = {
       ...config.resolve.fallback,
       crypto: require.resolve('crypto-browserify'),
@@ -11,7 +37,25 @@ module.exports = {
       vm: require.resolve('vm-browserify'),
     };
 
-    // Add plugins for providing polyfills to the app
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@adorsys-gis/multiple-did-identities/src': path.resolve(
+        // eslint-disable-next-line no-undef
+        __dirname,
+        'node_modules/@adorsys-gis/multiple-did-identities/dist/src',
+      ),
+    };
+
+    if (config.module && Array.isArray(config.module.rules)) {
+      config.module.rules.forEach((rule) => {
+        if (rule.oneOf && Array.isArray(rule.oneOf)) {
+          rule.oneOf.forEach(modifyBabelLoader);
+        } else {
+          modifyBabelLoader(rule);
+        }
+      });
+    }
+
     config.plugins = [
       ...config.plugins,
       new webpack.ProvidePlugin({
