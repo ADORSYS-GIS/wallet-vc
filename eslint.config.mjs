d89c289ff5c856c4bf0ee2aced795223e9341663
ndefokou <tcheutchouaarthur38@gmail.com>
2025-04-01 11:34:27 +0100

    fix():updating deprecated deps

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+    },
+    rules: {
+      ...typescript.configs["recommended"].rules,
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    ignores: ["node_modules/", "dist/", "build/"],
+  },
+];
