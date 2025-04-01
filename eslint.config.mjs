394268554a8f8fa42b1994d2a511c5b639dbdb32
ndefokou <tcheutchouaarthur38@gmail.com>
2025-04-01 11:43:41 +0100

    fix():formating

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,25 @@
-import typescript from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
 export default [
   {
-    files: ["**/*.ts", "**/*.tsx"],
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
     },
     plugins: {
-      "@typescript-eslint": typescript,
+      '@typescript-eslint': typescript,
     },
     rules: {
-      ...typescript.configs["recommended"].rules,
-      "@typescript-eslint/no-explicit-any": "off",
+      ...typescript.configs['recommended'].rules,
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
   {
-    ignores: ["node_modules/", "dist/", "build/"],
+    ignores: ['node_modules/', 'dist/', 'build/'],
   },
 ];
